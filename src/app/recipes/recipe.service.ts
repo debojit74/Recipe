590453d7819ca1c3ffeadf67f61cdbe2fbf3823f
Debojit <debojits74@gmail.com>
2019-05-10 00:05:38 +0530

    routing

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('First Recipe',
+            'First test recipe',
+            'https://c.pxhere.com/images/2a/0a/fa25a1946f1597195a166b9d5df1-1459569.jpg!d',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Second Recipe',
+            'Second test recipe',
+            'https://c.pxhere.com/images/2a/0a/fa25a1946f1597195a166b9d5df1-1459569.jpg!d',
+            [
+                new Ingredient('Buns', 1),
+                new Ingredient('Tomato', 20)
+            ])
+    ];
+
+    constructor(private shoppingListService: ShoppingListService){}
+
+    getRecipes() {
+        return this.recipes.slice(); //return new array as copy not the above recipe array.
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
+    }
+}
