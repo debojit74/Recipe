d28e17bccd7971ba4e09a3f84a6966853c2c7782
Debojit <debojits74@gmail.com>
2019-05-16 01:11:13 +0530

    forms1

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -13,6 +15,10 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index: number){
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
@@ -20,6 +26,16 @@ export class ShoppingListService {
 
     addIngredients(ingredient: Ingredient[]){
         this.ingredients.push(...ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number){
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
